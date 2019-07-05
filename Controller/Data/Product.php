d007a8039853e73f3790ad9931394d56d96f9faf
Timon de Groot <timon@marissen.net>
2019-07-05 11:04:13 +0200

    Remove whitespace

--- Controller/Data/Product.php
+++ Controller/Data/Product.php
@@ -67,8 +67,6 @@ class Product extends Action
             throw new NotFoundException(__('Product %1 does not exist.', $productId));
         }
 
-
-
         return $this->jsonFactory->create()->setData([
             'id' => $product->getId(),
             'name' => $product->getName(),
