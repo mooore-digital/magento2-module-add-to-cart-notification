b1d6f6463dad315f6d091ebaa1dbf4d8e8e90ec9
Grimlink <sean.grimlink@gmail.com>
2020-07-09 14:40:48 +0200

    IMP: modal and message output

--- view/frontend/web/js/catalog/add-to-cart-mixin.js
+++ view/frontend/web/js/catalog/add-to-cart-mixin.js
@@ -1,11 +1,7 @@
-define([
-    'jquery',
-    'mage/translate',
-    'jquery/ui'
-], function ($) {
-    'use strict';
+define(["jquery", "mage/translate", "jquery/ui"], function ($) {
+    "use strict";
 
-    var config = window['checkout']['add_to_cart_notification'];
+    var config = window["checkout"]["add_to_cart_notification"];
 
     var mixin = {
         /**
@@ -17,46 +13,70 @@ define([
 
             this._super(form);
 
-            self.element.find('.message.error').remove();
+            self.element.find(".message.error").remove();
 
-            $.when(
-                this.productPromise(productId),
-                this.cartPromise()
-            ).then(function (data) {
-                var product = data[0];
-                var title = $.mage.__('Added To Bag');
-                var message = $.mage.__('You added %1 to your <a href="%2">shopping cart</a>.')
-                    .replace('%1', product['name'])
-                    .replace('%2', product['cart_url']);
+            $.when(this.productPromise(productId), this.cartPromise()).then(
+                function (data) {
+                    var product = data[0];
+                    var title = $.mage.__("Added To Bag");
+                    var message = $.mage.__('You added %1 to your <a href="%2">shopping cart</a>.')
+                            .replace("%1", product["name"])
+                            .replace("%2", product["cart_url"]);
 
-                $('body').append(
-                    '<div id="add_to_cart_notification" class="add-to-cart">' +
-                    '<div class="btn-close"></div>' +
-                    '<img class="notification-image" src="' + product['image'] + '" alt="' + product['name'] + '" title="' + product['name'] + '">' +
-                    '<div class="notification-content">' +
-                    '<p class="notification-title">' + title + '</p>' +
-                    '<p class="notification-message">' + message + '</p>' +
-                    '</div>' +
-                    '</div>'
-                );
+                    var modalCloseBtn =
+                        '<button class="action-close"><span>' + $.mage.__("Close") + "</span></button>";
 
-                $("#add_to_cart_notification").find(".btn-close").click(function () {
-                    self.removeNotification();
-                });
+                    // TODO: get width and height from product image xml config
+                    var modalImage =
+                        '<img class="notification-image" src="' +
+                        product["image"] +
+                        '" alt="' +
+                        product["name"] +
+                        '" width="80" height="80">';
 
-                setTimeout(function () {
-                    self.removeNotification();
-                }, config['notificationLifetime']);
-            }, function (messages) {
-                if (self.element.find('div.mage-error').filter(':visible').length > 0) {
-                    return;
+                    $("body").append(
+                        '<div id="add_to_cart_notification" role="dialog" class="modal-custom modal-added-to-cart">' +
+                        '<div class="modal-header">' +
+                        modalCloseBtn +
+                        "</div>" +
+                        '<div class="modal-content"><div class="product-image">' +
+                        modalImage +
+                        '</div><div class="product-content">' +
+                        '<div class="block-title">' +
+                        '<strong id="add_to_cart_notification-heading" role="heading" aria-level="2">' +
+                        title +
+                        "</strong></div>" +
+                        '<div class="notification-message">' +
+                        message +
+                        "</div>" +
+                        "</div></div></div>"
+                    );
+
+                    $("#add_to_cart_notification")
+                        .find(".action-close")
+                        .click(function () {
+                            self.removeNotification();
+                        });
+
+                    setTimeout(function () {
+                        self.removeNotification();
+                    }, config["notificationLifetime"]);
+                },
+                function (messages) {
+                    if (self.element.find("div.mage-error").filter(":visible").length > 0) {
+                        return;
+                    }
+
+                    var button = self.element.find("button.tocart");
+                    messages.forEach(function (message) {
+                        button.after(
+                            '<div class="message info message-add-to-cart"><div>' +
+                            message.text +
+                            "</div></div>"
+                        );
+                    });
                 }
-                
-                var button = self.element.find('button.tocart');
-                messages.forEach(function (message) {
-                    button.after('<p class="message error add-to-cart-error">' + message.text + '</p>');
-                });
-            });
+            );
         },
 
         removeNotification: function () {
@@ -67,7 +87,7 @@ define([
 
         productPromise: function (productId) {
             return $.get({
-                url: window['BASE_URL'] + '/cart_notification/data/product/product_id/' + productId,
+                url: window["BASE_URL"] + "/cart_notification/data/product/product_id/" + productId,
                 cache: true
             });
         },
@@ -75,19 +95,19 @@ define([
         cartPromise: function () {
             var dfd = $.Deferred();
 
-            $(document).on('ajaxSuccess', function (event, request, settings) {
-                if (settings.url.indexOf('/checkout/cart/add') === -1) {
+            $(document).on("ajaxSuccess", function (event, request, settings) {
+                if (settings.url.indexOf("/checkout/cart/add") === -1) {
                     return;
                 }
 
                 if (request.responseJSON.error_messages) {
                     dfd.reject(request.responseJSON.error_messages);
-                    $(document).off('ajaxSuccess');
+                    $(document).off("ajaxSuccess");
                     return;
                 }
 
                 dfd.resolve();
-                $(document).off('ajaxSuccess');
+                $(document).off("ajaxSuccess");
             });
 
             return dfd.promise();
@@ -95,12 +115,12 @@ define([
     };
 
     return function (target) {
-        if (!config['enabled']) {
+        if (!config["enabled"]) {
             return target;
         }
 
-        $.widget('mooore.catalogAddToCart', target, mixin);
+        $.widget("mooore.catalogAddToCart", target, mixin);
 
         return $.mooore.catalogAddToCart;
-    }
+    };
 });
