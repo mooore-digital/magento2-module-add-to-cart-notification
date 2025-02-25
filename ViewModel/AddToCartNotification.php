da3f0e1adc9040e945dcbbe989ad47559796a51b
Joshua <joshua@mooore.nl>
2025-02-25 15:32:03 +0100

    TYT-2427 IMP: add notification for hyva

---
+++ ViewModel/AddToCartNotification.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mooore\ImageFeed\ViewModel\ImageFeed;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class HyvaRenderer extends Renderer implements ArgumentInterface
+{
+    public function __construct(
+
+    ) {
+        //
+    }
+}
